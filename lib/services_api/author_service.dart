c1de8cf9c8a5c4fbdc6d7dd4a367a9a7b7c2f865
Bintang Villa Devara <11211024@student.itk.ac.id>
2024-04-17 15:57:52 +0800

    Create author_service.dart

---
+++ lib/services_api/author_service.dart
@@ -0,0 +1,6 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:get/get.dart';
+
+class AuthorController extends GetxController {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+}
